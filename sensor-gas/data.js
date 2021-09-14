dbf75459af0d553642dda7f7c6749e4edaf51b65
lprader <lrprader@gmail.com>
2021-09-14 12:46:09 -0700

    demo updates

--- sensor-gas/data.js
+++ sensor-gas/data.js
@@ -1,9 +1,9 @@
 export default {
 	name: "CUSTOM_NAME", 
-	type: "Humidity", 
-	icon: "HumidityIcon",
-	color: "Green",
-	units: "%", 
+	type: "Gas", 
+	icon: "GasIcon",
+	color: "Purple",
+	units: "PPM", 
 	min: MIN_VALUE,
 	max: MAX_VALUE,
 	interval: CUSTOM_INTERVAL
